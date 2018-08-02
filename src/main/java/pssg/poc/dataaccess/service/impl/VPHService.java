c1383ed032ebdc19f4332233598078a6a463327a
Echo Liang <echo.2.liang@gov.bc.ca>
2018-08-02 13:16:46 -0700

    resolved autowire issue

--- src/main/java/pssg/poc/dataaccess/service/impl/VPHService.java
+++ src/main/java/pssg/poc/dataaccess/service/impl/VPHService.java
@@ -3,11 +3,13 @@ package pssg.poc.dataaccess.service.impl;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import pssg.poc.dataaccess.dao.VPHDao;
 import pssg.poc.dataaccess.service.IVPHService;
 import pssg.poc.model.VPHTicket;
 
+@Service
 public class VPHService implements IVPHService {
 
 	@Autowired

adcecbf1b9a307d412ec19174a43a79e3fffc7c7
echo-liang <37156357+echo-liang@users.noreply.github.com>
2018-08-15 14:39:02 -0700

    change the rest path

--- src/main/java/pssg/poc/web/controller/rest/SCRestController.java
+++ src/main/java/pssg/poc/web/controller/rest/SCRestController.java
@@ -17,9 +17,30 @@ public class SCRestController {
 	/** The logger. */
 	private final Logger logger = LoggerFactory.getLogger(getClass());
 
-	@RequestMapping(value = "/greeting", method = RequestMethod.GET)
+	@RequestMapping(value = "/rest/PSSGVPHAPI/eVTTransmission/state", method = RequestMethod.GET)
     public String vphAdminTools(final Model model, final HttpServletRequest request) {
-    	logger.info("hello world");
-    	return "{'hello': 'world'}";
+    	logger.info("hello vph hub");
+    	return ("{\n" +
+                "\t\"interfaceInfo\": [{\n" +
+                "\t\t\"interfaceState\": \"RUNNING\",\n" +
+                "\t\t\"interfaceName\": \"ICBC_CC\",\n" +
+                "\t\t\"lastUpdatedDateTime\": \"2018-08-08 17:10:14.0\"\n" +
+                "\t},\n" +
+                "\t{\n" +
+                "\t\t\"interfaceState\": \"RUNNING\",\n" +
+                "\t\t\"interfaceName\": \"ICBC_QT\",\n" +
+                "\t\t\"lastUpdatedDateTime\": \"2018-07-26 15:21:47.0\"\n" +
+                "\t},\n" +
+                "\t{\n" +
+                "\t\t\"interfaceState\": \"RUNNING\",\n" +
+                "\t\t\"interfaceName\": \"ICBC_CR\",\n" +
+                "\t\t\"lastUpdatedDateTime\": \"2018-07-25 16:07:56.0\"\n" +
+                "\t},\n" +
+                "\t{\n" +
+                "\t\t\"interfaceState\": \"FAILED\",\n" +
+                "\t\t\"interfaceName\": \"VPH_FD\",\n" +
+                "\t\t\"lastUpdatedDateTime\": \"2018-08-10 09:12:54.0\"\n" +
+                "\t}]\n" +
+                "}");
     }
 }
